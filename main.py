e04067a0a39374f6e0b60a64efcbca79aa9d9835
Maksim Minin <maxim@moduleq.com>
2024-09-26 19:30:54 -0400

    Current server time

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
+from datetime import datetime
 
 app = FastAPI()
 
 app.mount("/www", StaticFiles(directory="www"), name="www")
+@app.get("/currentTime")
+async def currentTime():
+    results = {"timeValue": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+    return results
 
 @app.get("/", response_class=HTMLResponse)
 async def read_items():
